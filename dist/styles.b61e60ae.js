7857ba315dc031f136dc7023abbf077dd7bbbe56
Philos <philipp@philipp-liebhart.de>
2020-10-09 13:29:19 +0200

    jounral finalised and dashboard card added

--- dist/styles.b61e60ae.js
+++ dist/styles.b61e60ae.js
@@ -232,7 +232,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61680" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61624" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
