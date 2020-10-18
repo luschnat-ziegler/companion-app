27b9ef00bc4315a48f129a72a456eefc9b7587d7
Marian Luschnat-Ziegler <marian@luschnat.de>
2020-10-18 15:48:46 +0200

    Storybook sucks

---
+++ src/stories/2-Chart.stories.js
@@ -0,0 +1,155 @@
+export default {
+    title: "Chart"
+};
+
+export const chart = () => `<section class="section__journal fluid chart">
+<h2 class="section-title">Energy Level throughout the day</h2>
+<div class="layout-grid">
+  <div class="grid-cell page-subtitle grid-cell-left mt-1">
+    100
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell page-subtitle grid-cell-left">
+    75
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell page-subtitle grid-cell-left">
+    50
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell page-subtitle grid-cell-left">
+    25
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div id="div" class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell page-subtitle grid-cell-left"> 
+  0
+  </div>
+  <div class="grid-cell">
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell bar-start">
+    <div class="left-bar left-bar1"></div>
+    <div class="right-bar right-bar1"></div>
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell bar-start">
+    <div class="left-bar left-bar2"></div>
+    <div class="right-bar right-bar2"></div>
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell bar-start">
+    <div class="left-bar left-bar3"></div>
+    <div class="right-bar right-bar3"></div>
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell bar-start">
+    <div class="left-bar left-bar4"></div>
+    <div class="right-bar right-bar4"></div>
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell bar-start">
+    <div class="left-bar left-bar5"></div>
+    <div class="right-bar right-bar5"></div>
+    <div class="line"></div>
+  </div>
+  <div class="grid-cell"></div>
+  <div class="grid-cell"></div>
+  <div class="grid-cell page-subtitle grid-cell-bottom">
+    10:00
+  </div>
+  <div class="grid-cell page-subtitle grid-cell-bottom">
+    15:30
+  </div>
+  <div class="grid-cell page-subtitle grid-cell-bottom">
+    16:15
+  </div>
+  <div class="grid-cell page-subtitle grid-cell-bottom">
+    18:25
+  </div>
+  <div class="grid-cell page-subtitle grid-cell-bottom">
+    19:00
+  </div>
+</div>
+<div class="legend">
+  <div class="legend__tag legend__tag--left">Other Students</div>
+  <div class="legend__tag legend__tag--right">You</div>
+</div>
+</section>
+
+<section class="section__journal fluid">
+<h2 class="section-title dashboard-card-title">
+Rate your current energy level:
+</h2>
+<button class="btn__energy btn__energy--100 text-white">100%</button>
+<button class="btn__energy btn__energy--75 text-white">75%</button>
+<button class="btn__energy btn__energy--50 text-secondary">50%</button>
+<button class="btn__energy btn__energy--25 text-secondary">25%</button>
+</section>`;
---
+++ src/stories/3-Nav.stories.js
@@ -0,0 +1,38 @@
+export default {
+    title: "Navigation"
+};
+
+export const navBar = () => `<footer class="footer">
+<nav class="navbar" id="navBar">
+  <img
+    class="navbar__icon navbar__icon--active"
+    id="navDash"
+    src="img/DashboardIcon.svg"
+    alt="Dashboard Icon"
+  />
+  <img
+    class="navbar__icon"
+    id="navBuddy"
+    src="img/BuddyIcon.svg"
+    alt="Buddy Icon"
+  />
+  <img
+    class="navbar__icon"
+    id="navTeams"
+    src="img/TeamIcon.svg"
+    alt="Team Icon"
+  />
+  <img
+    class="navbar__icon"
+    id="navEnergy"
+    src="img/EnergyIcon.svg"
+    alt="Energy Icon"
+  />
+  <img
+    class="navbar__icon"
+    id="navJournal"
+    src="img/JournalIcon.svg"
+    alt="Journal Icon"
+  />
+</nav>
+</footer>`;
